134c1d712c62e535f312ed9bfe684ff43f037ccf
devlap <nascal3@gmail.com>
2018-12-28 18:31:14 +0300

    added testing onInit in getting items from service

--- src/app/todos/todos.component.spec.ts
+++ src/app/todos/todos.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { TodosComponent } from './todos.component';
 import { TodoService } from '../06-services/todo.service';
 import { HttpClientModule } from '@angular/common/http';
+import {from, Observable, of} from 'rxjs';
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
@@ -17,10 +18,16 @@ describe('TodosComponent', () => {
 
     fixture = TestBed.createComponent(TodosComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should load todos from the server', () => {
+    const todos = {'one': 1, 'two': 2};
+    const service = TestBed.get(TodoService);
+    spyOn(service, 'getTodos').and.callFake(() => {
+      return of(todos);
+    });
+
+    fixture.detectChanges();
+    expect(component.todos).toBe(todos);
   });
 });
