23451b6f98884d5ad57cde390ef7f19169ec6ad2
devlap <nascal3@gmail.com>
2018-12-28 16:19:33 +0300

    added components for intergation testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { VoterComponent } from './voter/voter.component';
+import { TodosComponent } from './todos/todos.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VoterComponent,
+    TodosComponent,
+    HomeComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
