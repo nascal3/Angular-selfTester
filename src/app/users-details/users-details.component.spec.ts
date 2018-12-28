ad0369a516bc151bced51416f03db53fd37db6db
devlap <nascal3@gmail.com>
2018-12-28 19:18:39 +0300

    test integration of routes redirection in component

--- src/app/users-details/users-details.component.spec.ts
+++ src/app/users-details/users-details.component.spec.ts
@@ -1,6 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UsersDetailsComponent } from './users-details.component';
+import {ActivatedRoute, Router} from '@angular/router';
+import {empty, Observable} from 'rxjs';
+
+class RouterStub {
+ navigate(params) {
+ }
+}
+
+class ActivatedRouteStub {
+  params: Observable<any> = empty();
+}
 
 describe('UsersDetailsComponent', () => {
   let component: UsersDetailsComponent;
@@ -8,7 +19,11 @@ describe('UsersDetailsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UsersDetailsComponent ]
+      declarations: [ UsersDetailsComponent ],
+      providers: [
+        {provide: Router, useClass: RouterStub},
+        {provide: ActivatedRoute, useClass: ActivatedRouteStub}
+      ]
     })
     .compileComponents();
   }));
@@ -19,7 +34,12 @@ describe('UsersDetailsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should redirect to the users page after saving', () => {
+    const router = TestBed.get(Router);
+    let spy = spyOn(router, 'navigate');
+
+    component.save();
+
+    expect(spy).toHaveBeenCalledWith(['users']);
   });
 });
--- src/app/users-details/users-details.component.ts
+++ src/app/users-details/users-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-users-details',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe(p => {
+      if (p['id'] === 0) {
+        this.router.navigate(['not-found']);
+      }
+    });
+  }
+
+  save() {
+    this.router.navigate(['users']);
   }
 
 }
